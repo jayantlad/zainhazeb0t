38760c081ac1d17845b8aa116fd42a112e5f4885
Jayant Lad <jayanttlad@gmail.com>
2021-01-04 22:21:33 +0000

    adding api

---
+++ Api/Apis.cs
@@ -0,0 +1,42 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Azure.Cosmos.Table;
+using System.Linq;
+
+namespace Api
+{
+    public static class Apis
+    {
+        [FunctionName("GetUrls")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            try
+            {
+                var tableServiceClient = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=zainhazebotstorageacc;AccountKey=rMs/w+qMLp8sBrpY2DV7aMAH7oWbqIJDskAhad0wQYt2AoJUh49pbFRjtbfdBPJaTcxr82L5+6FR1lTeaumLsg==;EndpointSuffix=core.windows.net");
+
+                var cloudTableClient = tableServiceClient.CreateCloudTableClient();
+
+                var urlsTable = cloudTableClient.GetTableReference("microbiturls");
+
+                var cvQuery = new TableQuery<Urls>();
+                var urls = urlsTable.ExecuteQuery(cvQuery);
+
+                return new OkObjectResult(new { MicroBitUrls = urls.ToList() });
+            }
+            catch (Exception e)
+            {
+                log.LogError(e.StackTrace);
+            }
+            return new BadRequestResult();
+        }
+    }
+}
---
+++ Api/Urls.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Api
+{
+    public class Urls : TableEntity
+    {
+        public string url { get; set; }
+        public string partitionKey { get; set; }
+        public string rowKey { get; set; }
+    }
+}
