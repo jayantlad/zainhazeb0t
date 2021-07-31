8409fe6944f76aa0a0e36235377d8147e32ceabb
Jayant Lad <jayanttlad@gmail.com>
2021-07-31 19:48:17 +0100

    refactoring

--- app/app.js
+++ app/app.js
@@ -1,6 +1,12 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-      message: 'Hello Vue!'
-    }
-  })
+export default {
+  name: "App",
+  data() {
+    return {
+      message: ""
+    };
+  },
+  async mounted() {
+    const { text } = await (await fetch("/api/message")).json();
+    this.message = text;
+  }
+};
